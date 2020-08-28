fdbca2a29cbddec72496bf931e95cb8c5c6e797c
abdullah637 <abdullahbd637@gmail.com>
2020-08-28 19:52:38 +0600

    react done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React  from 'react';
+
+import './App.css';
+import Subject from './Component/Subject/Subject';
+
+import Header from './Component/Header/Header';
+
+
+
+function App() {
+ 
+  
+
+  return (
+    <div >
+      <Header></Header>
+      <Subject></Subject>
+     
+     
+    </div>
+  );
+}
+
+export default App;
